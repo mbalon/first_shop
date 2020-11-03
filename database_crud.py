bc6ceb8b43cdde77cc699c584697bed8d43a495d
mbalon <balonmateusz@gmail.com>
2020-11-03 18:08:46 +0100

    make factory function

--- database_crud.py
+++ database_crud.py
@@ -1,48 +1,52 @@
+import pathlib
+from typing import Optional
+
 from flask import Flask
 from flask_sqlalchemy import request
 from flask.json import jsonify
 from models import db, Producer, Product
 
-app = Flask(__name__)
 
-# connect with database
-db_name = 'shop_24_10_20.db'
+# This is factory function
+def create_app(database_uri: Optional[str] = None) -> Flask:
+    app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+    if database_uri is None:
+        db_file_path = pathlib.Path(__file__).parent / "shop_24_10_20.db"
+        database_uri = f"sqlite:///{db_file_path}"
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_uri
 
-db.init_app(app)
+    db.init_app(app)
 
+    @app.route("/add_producer", methods=["POST"])
+    def add_producer():
+        data = request.get_json()
+        try:
+            producer = Producer(name=data["name"])
+            db.session.add(producer)
+            db.session.commit()
+            return jsonify(producer.to_dict())
+        except KeyError:
+            response = jsonify({"error": "Bad request", "missing field": "name"})
+            response.status_code = 400
+            return response
 
+    @app.route("/add_product", methods=["POST"])
+    def add_product():
+        data = request.get_json()
+        try:
+            product = Product(name=data["name"])
+            db.session.add(product)
+            db.session.commit()
+            return jsonify(product.to_dict())
+        except KeyError:
+            response = jsonify({"error": "Bad request", "missing field": "name"})
+            response.status_code = 400
+            return response
 
-@app.route("/add_producer", methods=["POST"])
-def add_producer():
-    data = request.get_json()
-    try:
-        producer = Producer(name=data["name"])
-        db.session.add(producer)
-        db.session.commit()
-        return jsonify(producer.to_dict())
-    except KeyError:
-        response = jsonify({"error": "Bad request", "missing field": "name"})
-        response.status_code = 400
-        return response
-
-
-@app.route("/add_product", methods=["POST"])
-def add_product():
-    data = request.get_json()
-    try:
-        product = Product(name=data["name"])
-        db.session.add(product)
-        db.session.commit()
-        return jsonify(product.to_dict())
-    except KeyError:
-        response = jsonify({"error": "Bad request", "missing field": "name"})
-        response.status_code = 400
-        return response
+    return app
 
 
 if __name__ == '__main__':
-    app.run()
+    create_app("sqlite:///shop_24_10_20.db").run()
