67a599f9e121a646471638922093410d7318dbdb
mbalon <balonmateusz@gmail.com>
2020-11-28 20:03:57 +0100

    add get_producers func (+tests)

--- database_crud.py
+++ database_crud.py
@@ -15,9 +15,15 @@ def create_app(database_uri: Optional[str] = None) -> Flask:
         db_file_path = pathlib.Path(__file__).parent / "shop_24_10_20.db"
         database_uri = f"sqlite:///{db_file_path}"
 
+    print(database_uri)
+
     app.config["SQLALCHEMY_DATABASE_URI"] = database_uri
 
     db.init_app(app)
+
+    @app.route("/get_producers", methods=["GET"])
+    def get_producers():
+        return jsonify([producer.to_dict() for producer in db.session.query(Producer)])
 
     @app.route("/add_producer", methods=["POST"])
     def add_producer():
@@ -44,6 +50,16 @@ def create_app(database_uri: Optional[str] = None) -> Flask:
             response = jsonify({"error": "Bad request", "missing field": "name"})
             response.status_code = 400
             return response
+
+    @app.route("/get_producer/<int:producer_id>", methods=["GET"])
+    def get_producer(producer_id):
+        producer = db.session.query(Producer).filter_by(id=producer_id).first_or_404
+        return jsonify(producer.to_dict())
+
+    @app.route("/get_product/<int:product_id>", methods=["GET"])
+    def get_product(product_id):
+        product = db.session.query(Product).filter_by(id=product_id).first_or_404
+        return jsonify(product.to_dict())
 
     return app
 
--- tests/test_database_crud.py
+++ tests/test_database_crud.py
@@ -27,6 +27,29 @@ def populate_db():
                            price=3000, description='to work'))
 
 
+def test_get_producers_endpoint_returns_status_code_200_ok(client):
+    response = client.get("/get_producers")
+    assert response.status_code == 200
+
+
 def test_add_producer_endpoint_returns_status_code_200_ok(client):
     response = client.post("/add_producer", json={"id": 3, "name": "HP"})
     assert response.status_code == 200
+
+
+def test_add_producer_endpoint_add_new_record_to_db(client, app):
+    with app.app_context():
+        populate_db()
+
+        client.post("/add_producer", json={"id": 3, "name": "HP"})
+
+    excepted_list = [
+        {"id": 1, "name": "Dell"},
+        {"id": 2, "name": "Lenovo"},
+        {"id": 3, "name": "HP"}
+    ]
+
+    response = client.get("/get_producers")
+
+    actual_list = sorted(response.json, key=lambda producer: producer["id"])
+    assert actual_list == excepted_list
